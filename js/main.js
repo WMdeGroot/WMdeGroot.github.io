7378a87df1ba17d9cee82fafa111f49bc10bd7cd
Thijs de Groot <302368249@student.rocmondriaan.nl>
2023-04-19 10:18:18 +0200

    js object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 let profileFeatures = {
-
+    emailAddress:"thijsje876@gmail.com",
+    userName:"tjBig",
+    birthDate:"4 march 2007",
+    biography:"pas",
+    profilePicture: "media/pfp.avif",
+    projects: "tictactoe, hoger/lager, bootstrapproject",
+    education: "mbo 4",
+    skills: "programming, learning, clutching",
+    bannerColor: "red",
+    socialMedia: "none"
 }
